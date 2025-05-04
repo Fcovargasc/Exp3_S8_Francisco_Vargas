aeb36ab2d79fbe926a34e31d1e44b2e8ab1d4c57
Fcovargasc <fr.vargasc@duocuc.cl>
2025-05-04 15:03:36 -0400

    Add files via upload

--- Exp3_S8_Francisco_Vargas.java
+++ Exp3_S8_Francisco_Vargas.java
@@ -18,13 +18,13 @@ public class Exp3_S8_Francisco_Vargas {
         int opcion2=0;
         int opcion3=0;
         Boolean opcion4=false;
-        int opcion5=0;
         double estudiante=0.90;
         double terceraEdad= 0.85;
         String nombres="";
         String lugarAsientos="1 2 3\n4 5 6\n7 8 9\n";
         int asientosDisponibles=9;
-        int asiento=0;
+        String asiento="";
+        String nuevoAsiento="";
         System.out.println("BIENVENIDOS AL CAFE MORO");
         do{
             
@@ -36,30 +36,35 @@ public class Exp3_S8_Francisco_Vargas {
          opcion= scanner.nextInt();
          scanner.nextLine();
             
-         //1. venta de asientos 2.actualizar informacion 3. reserva de asientos 4.info de promociones why not 5.salir
+         
             switch (opcion){
                 case 1 :
+                    //por ahora se ve todo funcionando bien , no olvidar tratar de solucionar el error de por que no quiere pescar numeros mayores a 3 
                     System.out.println("---Venta de asientos---");
                     if (opcion4==true) {
                 System.out.println("---BOLETA---");
                    for (int i = 0; i < asientos.size(); i++) {
                     //System.out.println((i+1)+"."+nombres.get(i)+ "- $"+precios.get(i));
                     System.out.println(informacion.get(i)+": "+asientos.get(i));
-                       System.out.println("Su Reserva a sido efectiva");
-                          }break;
+                       
+                          }
+                   System.out.println("Su Reserva a sido efectiva");
+                   opcion4=false;
+                   informacion.remove("ID Asiento");
+                   informacion.remove("Valor Total");
+                   informacion.remove("Nombre");
+                   asientos.remove(asiento);
+                   asientos.remove(total);
+                   asientos.remove(nombres);
+                   break;
             }
                     
-                    for (int i = 1; i <= asientosDisponibles; i++) {
-                    
-                System.out.print(i + " ");
-                if (i % 3 == 0) {
-                    System.out.println();
-                }
-            } do{
+                     
+                    System.out.println(lugarAsientos);     
                     System.out.println("Seleccione su asiento");
-                     asiento =scanner.nextInt();
+                     asiento =scanner.nextLine();
                     
-            }while(asiento<1 || asiento>9);
+            
                     asientos.add(asiento);
                     informacion.add("ID Asiento");
                     do{              
@@ -88,69 +93,80 @@ public class Exp3_S8_Francisco_Vargas {
                           
                          informacion.add("Nombre");
                          asientos.add(nombres);
-                         do{
-                         System.out.println("para pagar presione 1 para volver al menu principal precione 2");
-                         opcion3=scanner.nextInt();
-                         }while(opcion3>2 || opcion3<1);
+                        
                          
-                         if (opcion3==1) {
+                        
                              System.out.println("---BOLETA---");
                              for (int i = 0; i < asientos.size(); i++) {
+                                 
                             //System.out.println((i+1)+"."+nombres.get(i)+ "- $"+precios.get(i));
                             System.out.println(informacion.get(i)+": "+asientos.get(i));
                           }
-                    }   else if (opcion3==2){
+                    
                             informacion.remove("ID Asiento");
                             informacion.remove("Valor Total");
                             informacion.remove("Nombre");
                             asientos.remove(asiento);
                             asientos.remove(total);
                             asientos.remove(nombres);
-                             System.out.println("Redirigiendo al menu principal...");
-                    }
+                             System.out.println("...Redirigiendo al menu principal...");
+                    
                         
                        
                     break;
                 case 2:
-                    System.out.println("---Actualizar Informacion---");
+                    //ver la verificacion de asientos que si preciona el mismo asiento tirar un mensaje y  volver a pedir que pong auna opcion 
+                    //todo lo demas se ve funcionando bien
+                     System.out.println("---Actualizar Informacion---");
                     if (asientos.isEmpty()) {
-                        System.out.println("Primmero compre o reserve un asiento para poder Actualizar");
+                        System.out.println("Reserve un asiento para poder Actualizar");
                         break;
                     }
-                    
+                    System.out.println("");
                     System.out.println("---Actualizacion de asientos---");
+                    System.out.println("");
                     System.out.println("Ingrese el asiento a modificar");
-                    int numeroAsiento= scanner.nextInt();
+                    String numeroAsiento= scanner.nextLine();
+                    
+                    
                         //obtenemos el indice del elemento
                     int indexActualizar = asientos.indexOf(numeroAsiento);
                         
                     if (indexActualizar!=-1) {
+
+                       do{
                        System.out.println("Ingrese un nuevo asiento");
                        System.out.println(lugarAsientos);
-                       int nuevoAsiento=scanner.nextInt();
+                        nuevoAsiento=scanner.nextLine();
+                           if (asientos.contains(nuevoAsiento)) {
+                               System.out.println("Asiento no disponible");
+                               
+                           }
+                       }while(asientos.contains(nuevoAsiento));
                        //actualiza el elemento
                         asientos.set(indexActualizar, nuevoAsiento);
                         System.out.println("Asiento actualizado ");
+                        System.out.println("Nuevo Asiento: "+asientos.get(indexActualizar));
                         
                    }else {
                         System.out.println("Asiento no encontrado");
                         }
-                        
-                    
                     break;
                 case 3:
+                    //todo parece funcionar bien
                     System.out.println("---Reserva de asientos---");
-                    for (int i = 1; i <= asientosDisponibles; i++) {
-                    
-                System.out.print(i + " ");
-                if (i % 3 == 0) {
-                    System.out.println();
-                }
-            } do{
+                    System.out.println(lugarAsientos);
+             do{
                     System.out.println("Seleccione su asiento");
-                     asiento =scanner.nextInt();
+                    asiento =scanner.nextLine();
+                    if (asientos.contains(asiento)) {
+                        System.out.println("Asiento no disponible");
+                        
+                    }
+            }while(asientos.contains(asiento));
                     
-            }while(asiento<1 || asiento>9);
+                    
+            
                     asientos.add(asiento);
                     informacion.add("ID Asiento");
                     do{                    
@@ -180,15 +196,18 @@ public class Exp3_S8_Francisco_Vargas {
                          informacion.add("Nombre");
                          asientos.add(nombres);
                          do{
-                         System.out.println("para pagar presione 1 para volver al menu principal precione 2");
+                         System.out.println("para pagar presione 1 para hacer efectiva su reserva primero presione  2 para volver al menu principal");
+                         
                          opcion3=scanner.nextInt();
                          }while(opcion3>2 || opcion3<1);
                          
                          if (opcion3==1) {
                              System.out.println("---BOLETA---");
                              for (int i = 0; i < asientos.size(); i++) {
-                            //System.out.println((i+1)+"."+nombres.get(i)+ "- $"+precios.get(i));
+                            
                             System.out.println(informacion.get(i)+": "+asientos.get(i));
+                            
+                                 
                           }
                     }  else if(opcion3==2){
                              System.out.println("Redirigiendo al menu principal...");
@@ -197,11 +216,13 @@ public class Exp3_S8_Francisco_Vargas {
                    break;
                 case 4:
                     
-                    System.out.println("Descuento estudiante 10%");
-                    System.out.println("descuento tercera edad 15%");
-                    System.out.println("Entrada General: $10000");
+                    String[] info={"Descuento estudiante 10%","descuento tercera edad 15%","Entrada General: $10000"};
+                    System.out.println(info[0]);
+                    System.out.println(info[1]);
+                    System.out.println(info[2]);
                     break;
                 case 5:
+                    //uwu
                     break;
             }
         
